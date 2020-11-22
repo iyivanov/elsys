f891f4c145b02b9b0935c66a9b46d5201cbb853a
iyivanov <iyivanov@gmail.com>
2020-11-22 21:21:25 +0100

    Added hello world programme

--- 01-hello-world/hello-world.c
+++ 01-hello-world/hello-world.c
@@ -1,1 +1,18 @@
-// this is a sample for a programme written in C programming langueage
+// ==============================================================
+// ELSIS
+// ==============================================================
+// This is a sample programme written in C programming langueage.
+// ==============================================================
+
+#define PRINT_NEW_LINE printf("\n")
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World!");
+
+    PRINT_NEW_LINE;
+    PRINT_NEW_LINE;
+
+    return 0;
+}
