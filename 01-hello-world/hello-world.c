40606fde0174762d9989ca9682f19c1d88267cc0
iyivanov <iyivanov@gmail.com>
2020-11-29 13:37:50 +0100

    Added commands for dumping preprocessor output and ASM output

--- 01-hello-world/hello-world.c
+++ 01-hello-world/hello-world.c
@@ -4,12 +4,19 @@
 // This is a sample programme written in C programming langueage.
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
+//
+//  try compiling as
+//  gcc -E hello-world.c > hello-world-preprocessed.c
+//  gcc -S hello-world.c -o hello-world-preprocessed.asm
+//
 #define PRINT_NEW_LINE printf("\n")
 
 #include <stdio.h>
 
 int main() {
     printf("Hello World!");
+
+    PRINT_NEW_LINE;
 
     PRINT_NEW_LINE;
 
