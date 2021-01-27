6c3077f1f149a039b4b81d4f356c610cabdbfdfc
iyivanov <iyivanov@gmail.com>
2021-01-28 00:02:57 +0100

    calculate the power usage

--- school/zadacha_za_5.c
+++ school/zadacha_za_5.c
@@ -35,20 +35,41 @@ struct apartament_t
 // izchislqva konsumaciqta
 float calc_current_consumption(apartament_p apartament)
 {
+
+    float obshta_konsumacia = 0;
+
     if (apartament != NULL)
     {
-        for (apartament->razmer) 
+        for (int i = 0; i < apartament->razmer; i++) 
         {
-            if (raboti) 
+            if (apartament->elektrouredi[i].raboti) 
             {
-                obshta_konsumaciq = obshta_konsumaciq + konsumaciq;
+                obshta_konsumacia = obshta_konsumacia + apartament->elektrouredi[i].consumacia;
             }
         }
     }
+
+    return obshta_konsumacia;
 }
 
 int main()
 {
-    
+    struct apartament_t apartament;
+    apartament.razmer = 5;
+    apartament.elektrouredi[0].raboti = true;
+    apartament.elektrouredi[0].consumacia = 0.5;
+    apartament.elektrouredi[1].raboti = true;
+    apartament.elektrouredi[1].consumacia = 0.7;
+    apartament.elektrouredi[2].raboti = false;
+    apartament.elektrouredi[2].consumacia = 1.2;
+    apartament.elektrouredi[3].raboti = true;
+    apartament.elektrouredi[3].consumacia = 0.5;
+    apartament.elektrouredi[4].raboti = true;
+    apartament.elektrouredi[4].consumacia = 0.1;
+
+    float cons = calc_current_consumption(&apartament);
+
+    printf("\nobshta consumacia: %.2f\n", cons);
+
     return 0;
 }
